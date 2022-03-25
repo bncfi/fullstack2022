2345293d7a4619790d2ce22986a30d5050d95152
bncfi <buoyancyfi@gmail.com>
2022-03-25 19:10:38 +0100

    refactored

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,14 +1,27 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 
-const Country = ({country, apikey, setWeatherData, weatherData}) => {
-  const [lat, lon] = country.capitalInfo.latlng
-  
-  axios.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apikey+"&units=metric")
-  .then(response => {
-    setWeatherData(response.data)
-  })
+
+const Weather = ({country, weatherData}) => {
+
   console.log(weatherData)
+  return(
+    <div>
+      <h1>Weather in {country.capital}</h1>
+      <p>temperature  Celcius</p>
+    </div> 
+  )
+}
+
+const Country = ({country, apikey, setWeatherData}) => {
+
+    const city = country.capital
+    axios
+      .get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey+"&units=metric")
+      .then(response => {
+        setWeatherData(response.data)
+      })
+  console.log("joo")
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -22,61 +35,38 @@ const Country = ({country, apikey, setWeatherData, weatherData}) => {
         }
       </ul>
       <img alt={country.name.common} src={country.flags.png} />
-      <h1>Weather in {country.capital}</h1>
-      <p>temperature Celcius</p>
-      
     </div>
   )
 }
 
-const Button = ({handle, text}) => {
-  return <button onClick={handle}>{text}</button>
-}
 
-const CountryOneLine = ({country,buttonHandleCountry, isShow,countryToShow, apikey, setWeatherData, weatherData}) => {
-  if(isShow && countryToShow === country.name.common) {
-    return <Country key={country.name.common} country={country} apikey={apikey} setWeatherData={setWeatherData} weatherData={weatherData}/>
-  }else if(isShow === false) {
+const CountryOneLine = ({country,buttonHandleCountry}) => {
       return(
         <p key={country.name.common}>{country.name.common}
-        <button onClick={() => buttonHandleCountry(country.name.common)}>show</button>
+        <button onClick={() => buttonHandleCountry(country)}>show</button>
         </p> 
       ) 
-    }else {
-      return <></>
-    }
 }
 
-const FilteredCountries = ({searchWord, countries, buttonHandleCountry, isShow,countryToShow,apikey, setWeatherData, weatherData}) => {
-  const filteredCountries = countries.filter((country) => {
-    return country.name.common.toLowerCase().includes(searchWord.toLowerCase())
-  })
-  if (filteredCountries.length > 10) {
+const FiltCountries = ({filteredCountryData, setCountryToShow, buttonHandleCountry, handleWeather}) => {
+  if (filteredCountryData.length > 10) {
     return( 
       <p>Too many results</p>
     )
-  }else if (filteredCountries.length < 10 && filteredCountries.length > 1) {
+  }else if (filteredCountryData.length < 10 && filteredCountryData.length > 1) {
+    console.log("filtCount", filteredCountryData)
     return(
-      filteredCountries.map((country) => {
-       return <CountryOneLine 
-       key={country.name.common} 
-       country={country} 
-       buttonHandleCountry={buttonHandleCountry} 
-       isShow={isShow} 
-       countryToShow={countryToShow} 
-       apikey={apikey}
-       setWeatherData={setWeatherData}
-       weatherData={weatherData}/>
-      })
+      filteredCountryData.map(country => <CountryOneLine key={country.name.common} country={country} buttonHandleCountry={buttonHandleCountry}/>)
     )
-  }else if(filteredCountries.length === 1) {
-    console.log(filteredCountries)
-    return( 
-      filteredCountries.map((country) => <Country key={country.name.common} country={country} apikey={apikey} setWeatherData={setWeatherData} weatherData={weatherData}/>
-      )
+  }else if (filteredCountryData.length === 1) {
+    setCountryToShow(filteredCountryData[0])
+    return(
+      <></>
     )
   }else {
-    return(<p>No results</p>)
+    return(
+      <></>
+    )
   }
 }
 
@@ -84,43 +74,55 @@ function App() {
   const apikey = process.env.REACT_APP_API_KEY
   const [countries, setCountries] = useState([])
   const [searchWord, setSearchword] = useState('')
-  const [isShow, setIsShow] = useState(false)
   const [countryToShow, setCountryToShow] = useState('')
-  const [weatherData, setWeatherData] = useState({})
+  const [weatherData, setWeatherData] = useState()
+  const [filteredCountryData, setFilteredCountryData] = useState([])
 
-  const hook = () => {
+  const hookCountries = () => {
     axios.get('https://restcountries.com/v3.1/all')
     .then(response => {
       setCountries(response.data)
     })
   }
 
-  useEffect(hook,[])
+  useEffect(hookCountries,[])
+/*
+  if(countryToShow !== '') {
+    const city = countryToShow.capital
+    const hook = (city,apikey, setWeatherData) => {
+      axios
+      .get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey+"&units=metric")
+      .then(response => {
+        setWeatherData(response.data)
+      })
+    }
+    useEffect(hook(city,apikey, setWeatherData), [])
+  }
+  */
 
   const inputHandleSearch = (event) => {
-    setSearchword(event.target.value)
+    const searchWord = event.target.value
+    setSearchword(searchWord)
+    const filteredCountries = countries.filter((country) => {
+      return country.name.common.toLowerCase().includes(searchWord.toLowerCase())
+    })
+    setFilteredCountryData(filteredCountries)
     setCountryToShow('')
-    setIsShow(false)
   }
 
-  const buttonHandleCountry = (countryName) => {
-    setIsShow(true)
-    setCountryToShow(countryName)
+  const buttonHandleCountry = (country) => {
+    setCountryToShow(country)
   }
-  console.log(apikey)
+
+
   return (
     <div className="App">
         search for country <input value={searchWord} onChange={inputHandleSearch}/>
-        <FilteredCountries 
-        searchWord={searchWord} 
-        countries={countries} 
-        buttonHandleCountry={buttonHandleCountry} 
-        isShow={isShow} 
-        countryToShow={countryToShow}
-        apikey={apikey}
-        setWeatherData={setWeatherData}
-        weatherData={weatherData}
-        />
+        {countryToShow === ''
+        ? <FiltCountries filteredCountryData={filteredCountryData} setCountryToShow={setCountryToShow} buttonHandleCountry={buttonHandleCountry} />
+        : <Country country={countryToShow} apikey={apikey} setWeatherData={setWeatherData}/>
+        }
+      
     </div>
   );
 }
