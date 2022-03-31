a003ebea7bb6c9b2dfff5d8ef0651fdd6a898a89
bncfi <buoyancyfi@gmail.com>
2022-03-31 15:17:26 +0200

    countries final

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,10 +2,11 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 
-const Weather = ({country, weatherData,apikey, setWeatherData}) => {
+const Weather = ({countryToShow, weatherData,apikey, setWeatherData}) => {
+  
   /*
   const hookWeather = () => {
-    const city = country.capital
+    const city = countryToShow.capital
     console.log("city ", city)
       axios
       .get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey+"&units=metric")
@@ -13,36 +14,67 @@ const Weather = ({country, weatherData,apikey, setWeatherData}) => {
         setWeatherData(response.data)
     })
   }
-  useEffect(hookWeather, [apikey,country, setWeatherData])
+  useEffect(hookWeather, [countryToShow, apikey, setWeatherData])
   */
-  console.log(weatherData)
+
   return(
     <div>
-      <h1>Weather in {country.capital}</h1>
+      <h1>Weather in {countryToShow.capital}</h1>
       <p>temperature {weatherData.main.temp}  Celcius</p>
-      <img alt={country.capital} src={"http://openweathermap.org/img/wn/"+weatherData.icon.weather[1]+".png"} />
+      <img alt={countryToShow.capital} src={"http://openweathermap.org/img/wn/"+weatherData.icon.weather[1]+".png"} />
       <p>wind {weatherData.wind.speed} m/s</p>
     </div> 
   )
 }
 
-const Country = ({country, apikey, setWeatherData}) => {
+const Country = ({countryToShow, apikey,weatherData, setWeatherData}) => {
+  const hookWeather = () => {
+    const city = countryToShow.capital[0]
+      axios
+      .get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey+"&units=metric")
+      .then(response => {
+        setWeatherData(response.data)
+    })
+  }
+  useEffect(hookWeather, [countryToShow, apikey, setWeatherData])
 
-  return (
-    <div>
-      <h1>{country.name.common}</h1>
-      <p>capital {country.capital}</p>
-      <p>area {country.area}</p>
-      <b>languages</b>
-      <ul>
-        {Object.entries(country.languages).map((array)=> {
-          return <li key={array[1]}>{array[1]}</li>
-        })
-        }
-      </ul>
-      <img alt={country.name.common} src={country.flags.png} />
-    </div>
-  )
+  if (weatherData === undefined) {
+    return (
+      <div>
+        <h1>{countryToShow.name.common}</h1>
+        <p>capital {countryToShow.capital}</p>
+        <p>area {countryToShow.area}</p>
+        <b>languages</b>
+        <ul>
+          {Object.entries(countryToShow.languages).map((array)=> {
+            return <li key={array[1]}>{array[1]}</li>
+          })
+          }
+        </ul>
+        <img alt={countryToShow.name.common} src={countryToShow.flags.png} />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>{countryToShow.name.common}</h1>
+        <p>capital {countryToShow.capital}</p>
+        <p>area {countryToShow.area}</p>
+        <b>languages</b>
+        <ul>
+          {Object.entries(countryToShow.languages).map((array)=> {
+            return <li key={array[1]}>{array[1]}</li>
+          })
+          }
+        </ul>
+        <img alt={countryToShow.name.common} src={countryToShow.flags.png} />
+        <h1>Weather in {countryToShow.capital}</h1>
+        <p>temperature {weatherData.main.temp}  Celcius</p>
+        <img alt={countryToShow.capital} src={"http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+".png"} />
+        <p>wind {weatherData.wind.speed} m/s</p>
+      </div>
+    )
+  }
 }
 
 
@@ -54,7 +86,7 @@ const CountryOneLine = ({country,buttonHandleCountry}) => {
       ) 
 }
 
-const FiltCountries = ({filteredCountryData, setCountryToShow, buttonHandleCountry, handleWeather}) => {
+const FiltCountries = ({filteredCountryData, setCountryToShow, buttonHandleCountry}) => {
   if (filteredCountryData.length > 10) {
     return( 
       <p>Too many results</p>
@@ -91,17 +123,6 @@ function App() {
   }
   useEffect(hookCountries,[])
 
-  
-  const hookWeather = () => {
-    const city = countryToShow.capital
-    console.log("city ", city)
-      axios
-      .get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey+"&units=metric")
-      .then(response => {
-        setWeatherData(response.data)
-    })
-  }
-  useEffect(hookWeather, [apikey,countryToShow])
 
   const inputHandleSearch = (event) => {
     const searchWord = event.target.value
@@ -117,14 +138,13 @@ function App() {
     setCountryToShow(country)
   }
 
-
+  //,  <Weather key={countryToShow.capital} weatherData={weatherData} countryToShow={countryToShow} apikey={apikey} setWeatherData={setWeatherData}/>
   return (
     <div className="App">
         search for country <input value={searchWord} onChange={inputHandleSearch}/>
         {countryToShow === ''
         ? <FiltCountries filteredCountryData={filteredCountryData} setCountryToShow={setCountryToShow} buttonHandleCountry={buttonHandleCountry} />
-        : [<Country key={countryToShow} country={countryToShow} apikey={apikey} setWeatherData={setWeatherData}/>, 
-          <Weather key={countryToShow.capital} weatherData={weatherData} country={countryToShow} apikey={apikey} setWeatherData={setWeatherData}/>]
+        : [<Country key={countryToShow} countryToShow={countryToShow} apikey={apikey} setWeatherData={setWeatherData} weatherData={weatherData}/>]
         }
       
     </div>
