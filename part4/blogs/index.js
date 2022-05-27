8432b52dfc0069e1192b91c0f002577757fd0cca
bncfi <buoyancyfi@gmail.com>
2022-05-27 20:33:12 +0300

    exercise 4.2 finished

--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -7,12 +7,10 @@ const logger = require('./utils/logger.js')
 const config = require('./utils/config.js')
 const blogsRouter = require('./controllers/blogs')
 
-app.use('/api/blogs', blogsRouter)
-
-mongoose.connect(config.MONGODB_URI)
-
 app.use(cors())
 app.use(express.json())
+mongoose.connect(config.MONGODB_URI)
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
