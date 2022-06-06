9a6ccfe3d990e693a027603b457ff156e93e0761
bncfi <buoyancyfi@gmail.com>
2022-06-06 13:06:10 +0300

    supertest

---
+++ part4/blogs/tests/blog_api.test.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Eka',
+    author: 'Aleksis Kivi',
+    url: 'buu',
+    likes: 3,
+  },
+  {
+    title: 'Toka postaus',
+    author: 'Iso H',
+    url: 'jee',
+    likes: 0,
+  },
+  {
+    title: 'Kolmas postaus',
+    author: 'Tommy Tabermann',
+    url: 'runoja',
+    likes: 1,
+  },
+]
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
+})
+
+test('three blogs are returned as json', () => {
+  const request = api
+    .get('/api/blogs')
+    .then((response) => console.log('huhuu', response.body))
+    .catch((error) => console.log(error))
+  console.log(request)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
