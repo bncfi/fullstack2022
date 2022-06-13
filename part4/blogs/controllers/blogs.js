7b0213f726e7c8e49ae0b26402dcb3df5fe5beb4
bncfi <buoyancyfi@gmail.com>
2022-06-13 10:58:32 +0300

    part4b tests

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -10,9 +10,48 @@ blogsRouter.get('/', (request, response) => {
 blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result)
+    })
+    .catch((error) => {
+      response.status(400).end()
+    })
+})
+
+/*
+blogsRouter.delete('/:id', (request, response) => {
+  
+  Blog.findByIdAndRemove(request.params.id)
+    .then(() => response.status(204).end())
+    .catch((error) => {
+      response.status(404).end()
+    })
+})
+*/
+
+blogsRouter.delete('/:id', async (request, response) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    response.status(400).end()
+  }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const updatedData = request.body
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      updatedData,
+      { new: true }
+    )
+    response.json(updatedBlog).status(200).end()
+  } catch (exception) {
+    response.status(400).end()
+  }
 })
 
 module.exports = blogsRouter
--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -35,17 +35,90 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-/*
-  const request = api
-    .get('/api/blogs')
-    .then((response) => expect(response.body).toHaveLength(initialBlogs.length))
-    .catch((error) => console.log(error))
-  return request
-  */
+describe('api get', () => {
+  test('three blogs are returned as json', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
 
-test('three blogs are returned as json', async () => {
-  const response = await api.get('api/blogs')
-  response.expect(response.body).toHaveLength(initialBlogs.length)
+  test('returned ids are correct', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach((blog) => {
+      expect(blog.id).toBeDefined()
+    })
+  })
+})
+
+describe('addition of a blog', () => {
+  test('new blog can be added', async () => {
+    const newBlog = {
+      title: 'Neljas',
+      author: 'Mestari Kirjailija',
+      url: 'dfas',
+      likes: 7,
+    }
+
+    await api.post('/api/blogs').send(newBlog)
+    const getResponse = await api.get('/api/blogs')
+
+    expect(getResponse.body).toHaveLength(initialBlogs.length + 1)
+    expect(getResponse.body.map((blog) => blog.title)).toContain('Neljas')
+  })
+
+  test('new blog without likes default to zero', async () => {
+    const newBlog = {
+      title: 'Neljas Liketon',
+      author: 'Mika Waltari',
+      url: 'eee',
+    }
+
+    const postResponse = await api.post('/api/blogs').send(newBlog)
+    expect(postResponse.body.likes).toBe(0)
+  })
+
+  test('new blog without title or url returns 400', async () => {
+    const newBlogWithoutTitle = {
+      author: 'Mika Waltari',
+      url: 'eee',
+    }
+    const newBlogWithoutUrl = {
+      title: 'Neljas Liketon',
+      author: 'Mika Waltari',
+    }
+
+    await api.post('/api/blogs').send(newBlogWithoutTitle).expect(400)
+    await api.post('/api/blogs').send(newBlogWithoutUrl).expect(400)
+  })
+})
+
+describe('deletion of a blog', () => {
+  test('deletion of a single blog returns 204', async () => {
+    const blogsAtStart = await api.get('/api/blogs')
+    const blogToDelete = blogsAtStart.body[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAtEnd = await api.get('/api/blogs')
+    expect(blogsAtEnd.body).toHaveLength(blogsAtStart.body.length - 1)
+  })
+
+  test('deletion of a wrong id returns 400', async () => {
+    await api.delete('/api/blogs/300').expect(400)
+  })
+})
+
+describe('update of a blog', () => {
+  test('update of likes returns 200 and likes are correct', async () => {
+    const blogs = await api.get('/api/blogs')
+    const blogToUpdate = blogs.body[0]
+    const updatedLikes = { likes: blogToUpdate.likes + 1 }
+
+    const updatedBlog = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedLikes)
+      .expect(200)
+    expect(updatedBlog.body.likes).toBe(updatedLikes.likes)
+  })
 })
 
 afterAll(() => {
