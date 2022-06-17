8a6ebd2d744f3c8ca8e929baf3a465675e6adc5d
bncfi <buoyancyfi@gmail.com>
2022-06-17 10:43:41 +0300

    backend refactor

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -3,60 +3,50 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  try {
-    const blogs = await Blog.find({}).populate('user', {
-      username: 1,
-      name: 1,
-      id: 1,
-    })
-    response.status(200).json(blogs)
-  } catch (error) {
-    response.status(404).json(error.message).end()
-  }
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+    id: 1,
+  })
+  response.status(200).json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  try {
-    const user = await User.findOne()
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes,
-      user: user.id,
-    })
 
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog.id)
-    await user.save()
-    response.status(201).json(savedBlog)
-  } catch (error) {
-    response.status(400).json(error.message).end()
+  if (!body.title || !body.url) {
+    response.status(400).json({ error: 'tittle or url missing' })
   }
+
+  const user = await User.findOne()
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog.id)
+  await user.save()
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  try {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (error) {
-    response.status(400).json(error.message).end()
-  }
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 blogsRouter.put('/:id', async (request, response) => {
   const updatedData = request.body
-  try {
-    const updatedBlog = await Blog.findByIdAndUpdate(
-      request.params.id,
-      updatedData,
-      { new: true }
-    )
-    response.json(updatedBlog).status(200).end()
-  } catch (error) {
-    response.status(400).json(error.message).end()
-  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    updatedData,
+    { new: true }
+  )
+  response.json(updatedBlog).status(200).end()
 })
 
 module.exports = blogsRouter
