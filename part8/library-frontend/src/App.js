25a1b1af197a94b3393cfc739c907df4d67d60dd
bncfi <buoyancyfi@gmail.com>
2023-02-08 12:56:25 +0200

    8.8 beginning

---
+++ part8/library-frontend/src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from './queries/queries'
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+  const authors = useQuery(ALL_AUTHORS)
+  console.log('app ', authors)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors show={page === 'authors'} />
+
+      <Books show={page === 'books'} />
+
+      <NewBook show={page === 'add'} />
+    </div>
+  )
+}
+
+export default App
---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,37 @@
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries/queries'
+
+const Authors = (props) => {
+  const authors = useQuery(ALL_AUTHORS)
+  console.log(authors)
+
+  if (!props.show) {
+    return null
+  }
+
+  //const authors = []
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
