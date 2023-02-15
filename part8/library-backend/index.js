d2ade32528f56b0eed0aea28def45f67d4b80be9
bncfi <buoyancyfi@gmail.com>
2023-02-15 12:18:10 +0200

    part8 c

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { GraphQLError } = require('graphql')
 const jwt = require('jsonwebtoken')
@@ -49,20 +49,6 @@ let authors = [
     id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
   },
 ]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
- */
 
 let books = [
   {
@@ -116,7 +102,7 @@ let books = [
   },
 ]
 
-const typeDefs = gql`
+const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
@@ -133,7 +119,7 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: String!
+    author: Author!
     id: ID!
     genres: [String!]!
   }
@@ -151,12 +137,25 @@ const typeDefs = gql`
 
 const resolvers = {
   Mutation: {
-    addBook: (root, args) => {
-      const book = { ...args, id: uuid() }
-      books = books.concat(book)
-      if (!authors.includes(book.author)) {
-        const author = { name: book.author, bookCount: 1 }
-        authors = authors.concat(author)
+    addBook: async (root, args) => {
+      console.log('testi: ', Author.find({ name: args.author }))
+      if (!Author.find({ name: args.author })) {
+        const author = new Author({ name: args.author })
+        author.save()
+      }
+      const author = Author.find({ name: args.author })
+      console.log(author)
+      const book = new Book({ ...args, author: author })
+      try {
+        book.save()
+      } catch (error) {
+        throw new GraphQLError('Saving book failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error,
+          },
+        })
       }
       return book
     },
@@ -171,11 +170,11 @@ const resolvers = {
     },
   },
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
       if (Object.keys(args).length === 0) {
-        return books
+        return Book.find({})
       }
       if (args.author && args.genre) {
         return books
@@ -183,15 +182,21 @@ const resolvers = {
           .filter((book) => book.genres.includes(args.genre.toLowerCase()))
       }
       if (args.author) {
-        return books.filter((book) => book.author === args.author)
+        return Book.find({ author: args.author })
       }
+      /*
+      if (args.author) {
+        return books.filter((book) => book.author === args.author)
+      }*/
       if (args.genre) {
         return books.filter((book) =>
           book.genres.includes(args.genre.toLowerCase())
         )
       }
     },
-    allAuthors: () => authors,
+    allAuthors: async () => {
+      return Author.find({})
+    },
   },
 
   Author: {
