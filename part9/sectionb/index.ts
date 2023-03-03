50318a067bdc22a4377e0d7e40705e6feb4ff8e5
bncfi <buoyancyfi@gmail.com>
2023-03-03 12:49:32 +0200

    9.5

--- part9/sectionb/index.ts
+++ part9/sectionb/index.ts
@@ -1,8 +1,23 @@
 import express from 'express'
+//import { BmiValues } from './bmiCalculator'
+import { calculateBmi } from './bmiCalculator'
 const app = express()
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => {
+  console.log(req.body)
+  const height = Number(req.query.height)
+  const weight = Number(req.query.weight)
+  if (!isNaN(height) && !isNaN(weight)) {
+    const bmi = calculateBmi(height, weight)
+    const bmiResponse = { height: height, weight: weight, bmi: bmi }
+    res.status(200).send(bmiResponse)
+  } else {
+    res.status(400).send('malformatted parameters')
+  }
 })
 
 const PORT = 3003
